e5a61ab69f37f3f4655e2d6f002914c58d20aabe
CaiqueBAR <159191424+CaiqueBAR@users.noreply.github.com>
2026-02-27 07:42:38 -0300

    Add files via upload

---
+++ Gestão de RH de uma Empresa/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+class Program
+{
+    
+    class Funcionario
+    {
+       
+        private int matricula;
+        private string nome;
+        private string cargoAtual;
+        private float salarioAtual;
+
+       
+
+        public void Cadastrar()
+        {
+            Console.WriteLine("Método cadastrar() executado.");
+        }
+
+        public void Consultar()
+        {
+            Console.WriteLine("Método consultar() executado.");
+        }
+
+        public void AtualizarSalario(float novoSalario)
+        {
+            Console.WriteLine($"Método atualizarSalario() executado. Novo salário: {novoSalario}");
+        }
+
+        public void ListarDependentes()
+        {
+            Console.WriteLine("Método listarDependentes() executado.");
+        }
+    }
+
+    static void Main()
+    {
+        Funcionario f = new Funcionario();
+
+        f.Cadastrar();
+        f.Consultar();
+        f.AtualizarSalario(4200.75f);
+        f.ListarDependentes();
+
+        Console.ReadKey();
+    }
+}
